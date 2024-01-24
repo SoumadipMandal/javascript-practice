001cbd2cc00fa3084ad52d82f1eef76897721084
Soumadip Mandal <155660687+SoumadipMandal@users.noreply.github.com>
2024-01-24 13:05:07 +0000

    conversion of detatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail ="crazzdip@gmail.com";
+var accountPassword = "12345"
+accountCity = "bengal"
+
+// accountId =2 ;
+accountEmail = "soumadip.com"
+accountPassword ="23456"
+accountCity ="kolkata"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,18 @@
+let score = "33"
+
+// console.log(typeof(score))
+
+let valueInNumber = Number (score);
+
+// console.log(typeof(valueInNumber));
+
+// "33" => 33 (convert)
+// "3abc" => NAN (convert)
+// true => 1 , false=> 0 ;
+
+// let isLoggedIn = 1;
+// booleanIsLoggedIn = Boolean(isLoggedIn)
+
+let someNumber = 33;
+let StringNumber = String(someNumber)
+console.log(typeof StringNumber);
